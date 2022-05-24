6fd083caacdf8f9744c6152e473a2055f309c417
radu-filipescu <radu.filipescu@s.unibuc.ro>
2022-05-24 03:36:57 +0300

    0.755

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
@@ -58,50 +59,63 @@ test_inputs = np.array(test_inputs)
 input_file.close()
 
 # convert string input to vectors, using features of sklearn library
-#vectorizer = CountVectorizer()
-vectorizer = TfidfVectorizer()
+vectorizer = CountVectorizer(analyzer="char", ngram_range=(1,8))
+#vectorizer = TfidfVectorizer(analyzer="char", ngram_range=(1,8))
 vectorizer.fit(training_input)
 
 training_input_vectorized = vectorizer.transform(training_input)
 validation_inputs_vectorized = vectorizer.transform(validation_inputs)
 test_inputs_vectorized = vectorizer.transform(test_inputs)
 
-### MULTINOMIAL NATIVE BAYES
+### CATEGORICAL NATIVE BAYES
+NBModel1 = BernoulliNB()
+NBModel1.fit(training_input_vectorized, training_labels)
+#prediction1 = NBModel1.predict(test_inputs_vectorized)
+#precision = NBModel1.score(validation_inputs_vectorized, validation_labels)
 
-#NBModel = MultinomialNB()
-#NBModel.fit(training_input_vectorized, training_labels)
-#prediction = NBModel.predict(test_inputs_vectorized)
-#precision = NBModel.score(validation_inputs_vectorized, validation_labels)
+### MULTINOMIAL NATIVE BAYES
+NBModel2 = MultinomialNB()
+NBModel2.fit(training_input_vectorized, training_labels)
+#prediction2 = NBModel2.predict(test_inputs_vectorized)
+#precision = NBModel2.score(validation_inputs_vectorized, validation_labels)
 
 ### COMPLEMENTAL NATIVE BAYES
-
-NBModel = ComplementNB()
-NBModel.fit(training_input_vectorized, training_labels)
-#prediction = NBModel.predict(test_inputs_vectorized)
-precision = NBModel.score(validation_inputs_vectorized, validation_labels)
-
-### SUPPORT VECTOR MACHINES
-
-#validation_inputs_vectorized = scaler.transform(validation_inputs_vectorized)
-
-# SVCModel = SVC(kernel='linear', gamma='scale', shrinking=True)
-# SVCModel.fit(training_input_vectorized, training_labels)
-# prediction = SVCModel.predict(test_inputs_vectorized)
-# precision = SVCModel.score(validation_inputs_vectorized, validation_labels)
+NBModel3 = ComplementNB()
+NBModel3.fit(training_input_vectorized, training_labels)
+prediction3 = NBModel3.predict(test_inputs_vectorized)
+precision = NBModel3.score(validation_inputs_vectorized, validation_labels)
 
 print(precision)
 
+# final_prediction = []
+# for i in range(len(prediction1)):
+#     votes = [0, 0, 0, 0]
+#     votes[prediction1[i]] += 1
+#     votes[prediction2[i]] += 1
+#     votes[prediction3[i]] += 1
+#
+#     max_votes = max(votes[1], votes[2], votes[3])
+#
+#     if max_votes == votes[1]:
+#         final_prediction.append(1)
+#     elif max_votes == votes[2]:
+#         final_prediction.append(2)
+#     else:
+#         final_prediction.append(3)
+
+prediction = prediction3
+
 # output to some file
 
-# output_file = open("output/output_file.txt", "w", encoding="utf-8")
-# output_file.write("id,label\n")
-#
-# for i in range(len(prediction)):
-#    output_file.write(test_Ids[i] + ',' + str(prediction[i]))
-#    if i < len(prediction) - 1:
-#        output_file.write('\n')
-#
-# output_file.close()
+output_file = open("output/output_file.txt", "w", encoding="utf-8")
+output_file.write("id,label\n")
+
+for i in range(len(prediction)):
+   output_file.write(test_Ids[i] + ',' + str(prediction[i]))
+   if i < len(prediction) - 1:
+       output_file.write('\n')
+
+output_file.close()
 
 
 
